9798a89dcb852d0b35763746d249cfbf2ddcd6f2
shaan sisodia <shaansisodia@MacBookPro.lan>
2026-03-05 15:58:28 +0700

    fix: reset to centered 8, 6

--- src/components/office-3d/Scene3D.tsx
+++ src/components/office-3d/Scene3D.tsx
@@ -15,7 +15,7 @@ import { ParentChildLine } from "./ParentChildLine";
 
 function IsometricOffice() {
   const { scene } = useGLTF("/isometric_office.glb");
-  return <primitive object={scene} scale={1} position={[0, 0.5, 6]} />;
+  return <primitive object={scene} scale={1} position={[8, 0.5, 6]} />;
 }
 
 function WaterPlane() {
@@ -30,7 +30,7 @@ function SunsetIsland() {
 
 function OfficePlatform() {
   return (
-    <group position={[0, -2, 6]}>
+    <group position={[8, -2, 6]}>
       <mesh castShadow receiveShadow>
         <cylinderGeometry args={[8, 10, 1, 32]} />
         <meshStandardMaterial color="#c4a882" roughness={0.8} />
@@ -43,7 +43,7 @@ function OfficePlatform() {
   );
 }
 
-const SCENE_CENTER: [number, number, number] = [0, 4, 6];
+const SCENE_CENTER: [number, number, number] = [8, 4, 6];
 const BG_LIGHT = new THREE.Color("#e8ecf2");
 const BG_DARK = new THREE.Color("#0f1729");
 
