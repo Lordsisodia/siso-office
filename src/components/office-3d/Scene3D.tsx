552eece6f9e27cb6ecc19792db2b1dbae49c7d3e
shaan sisodia <shaansisodia@MacBookPro.lan>
2026-03-05 16:41:40 +0700

    fix: move office Z to -10

--- src/components/office-3d/Scene3D.tsx
+++ src/components/office-3d/Scene3D.tsx
@@ -15,7 +15,7 @@ import { ParentChildLine } from "./ParentChildLine";
 
 function IsometricOffice() {
   const { scene } = useGLTF("/isometric_office.glb");
-  return <primitive object={scene} scale={1} position={[15, 0.5, 6]} />;
+  return <primitive object={scene} scale={1} position={[15, 0.5, -10]} />;
 }
 
 function WaterPlane() {
@@ -30,7 +30,7 @@ function SunsetIsland() {
 
 function OfficePlatform() {
   return (
-    <group position={[15, -2, 6]}>
+    <group position={[15, -2, -10]}>
       <mesh castShadow receiveShadow>
         <cylinderGeometry args={[8, 10, 1, 32]} />
         <meshStandardMaterial color="#c4a882" roughness={0.8} />
@@ -43,7 +43,7 @@ function OfficePlatform() {
   );
 }
 
-const SCENE_CENTER: [number, number, number] = [15, 4, 6];
+const SCENE_CENTER: [number, number, number] = [15, 4, -10];
 const BG_LIGHT = new THREE.Color("#e8ecf2");
 const BG_DARK = new THREE.Color("#0f1729");
 
